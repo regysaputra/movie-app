9e351c536390759795efd9e646b370a20a086f9a
Regy Saputra <regysaputra27@gmail.com>
2021-08-24 18:34:22 +0700

    update security

--- core/src/main/java/com/bangkit/batikapp/core/di/DatabaseModule.kt
+++ core/src/main/java/com/bangkit/batikapp/core/di/DatabaseModule.kt
@@ -9,6 +9,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,10 +19,16 @@ class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context): BatikDatabase = Room.databaseBuilder(
-        context,
-        BatikDatabase::class.java, "Batik.db"
-    ).fallbackToDestructiveMigration().build()
+    fun provideDatabase(@ApplicationContext context: Context): BatikDatabase {
+
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("bangkit".toCharArray())
+        val factory = SupportFactory(passphrase)
+
+        return Room.databaseBuilder(
+            context,
+            BatikDatabase::class.java, "Batik.db"
+        ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
+    }
 
     @Provides
     fun provideTourismDao(database: BatikDatabase): BatikDao = database.batikDao()
--- core/src/main/java/com/bangkit/batikapp/core/di/NetworkModule.kt
+++ core/src/main/java/com/bangkit/batikapp/core/di/NetworkModule.kt
@@ -1,3 +1,7 @@
+@file:Suppress("SpellCheckingInspection", "SpellCheckingInspection", "SpellCheckingInspection",
+    "SpellCheckingInspection", "SpellCheckingInspection", "SpellCheckingInspection"
+)
+
 package com.bangkit.batikapp.core.di
 
 import com.bangkit.batikapp.core.data.source.remote.network.ApiService
@@ -5,6 +9,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -17,10 +22,17 @@ class NetworkModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .build()
+
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
